53b4de936ef0875d7ef51807fe4bc32485e67ee3
juanmorenoHEIG <juan.morenoizquierdo@heig-vd.ch>
2018-10-08 16:36:03 +0200

    pour marina <3

--- models/user.js
+++ models/user.js
@@ -3,7 +3,15 @@ const Schema = mongoose.Schema;
 
 // Define the schema for users
 const userSchema = new Schema({
-  name: String
+  firstname: String,
+  name: String,
+  email: String,
+  password: String,
+  address: {
+    street: String,
+    NPA: Number,
+    City: String
+  }
 });
 // Create the model from the schema and export it
 module.exports = mongoose.model('User', userSchema);
--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,33 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.delete('/:id', function(req, res, next) {
+  User.findById(req.params.id).exec(function(err, user) {
+    if (err) { return next(err); }
+    else if (!user) { return res.sendStatus(404); }
+    user.remove(function(err,deleteduser){
+      if (err) {
+        return next(err);
+      }
+
+      res.sendStatus(204);
+    });
+  });
+});
+
+/* POST new user */
+router.post('/', function(req, res, next) {
+  // Create a new document from the JSON in the request body
+  const newUser = new User(req.body);
+  // Save that document
+  newUser.save(function(err, savedUser) {
+    if (err) {
+      return next(err);
+    }
+    // Send the saved document in the response
+    res.send(savedUser);
+  });
+});
+
+
 module.exports = router;
