3bb9f9fca37c4be119962c59f38b902ffaeae292
Marina Popov <marina.popov@heig-vd.ch>
2018-10-15 14:46:13 +0200

    order

--- models/order.js
+++ models/order.js
@@ -5,7 +5,7 @@ const OrderSchema = new Schema({
     date: { type: Date, default: Date.now },
     pickup_date: {type: Date},
     commandLine:     {
-        productId: {type: ObjectId},
+        productId: {type: Schema.Types.ObjectId},
         quantity: {type: Number}
     },
     userId: {type: ObjectId}
