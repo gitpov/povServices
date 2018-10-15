eafd59b98975a2ef605fce9f9b198ed9cf973802
audrey.huguenin <audrey.huguenin@heig-vd.ch>
2018-10-15 13:47:52 +0200

    add bcryptjs

--- models/order.js
+++ models/order.js
@@ -4,6 +4,11 @@ const Schema = mongoose.Schema;
 const OrderSchema = new Schema({
     date: { type: Date, default: Date.now },
     pickup_date: {type: Date},
+    commandLine:     {
+        productId: {type: ObjectId},
+        quantity: {type: Number}
+    },
+    userId: {type: ObjectId}
 });
 
 
