8e33d5924b4d1b2c398e40b267696c9fd4a50fc8
juanmorenoHEIG <juan.morenoizquierdo@heig-vd.ch>
2018-11-01 13:28:46 +0100

    add env to heroku

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,13 +1,15 @@
 define({
-  "name": "povservices-git-branching",
-  "version": "0.0.0",
-  "description": "",
+  "name": "AçaiGo",
+  "version": "1.0.0",
+  "description": "AçaiGo est un projet d'étudiants en troisième année de l'HEIG-VD. Il consiste à vendre des plats à l'emporter à base d'Açai. Le client a la possibilité d'ajouter des suppléments (Fruits, céréales, etc). Cette API permet la création, la modification et l'envoie de données pour le fonctionnemnt de la plateforme d'AçaiGo.",
+  "title": "AçaiGo",
+  "url": "https://acaigo.com",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-10-31T19:51:04.378Z",
+    "time": "2018-10-31T20:00:51.457Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
