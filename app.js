61b258e31b60cd0630904a5a679b3750520ee3ce
juanmorenoHEIG <juan.morenoizquierdo@heig-vd.ch>
2019-01-14 16:24:33 +0100

    err

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.post('/login', function(req, res, next){
         if(bcrypt.compareSync(req.body.password, user.password)){
             const payload = {id:user.id};
             const token = jwt.sign(payload, process.env.SECRET_KEY);
-            res.send({token:token},{user:user});
+            res.send({token:token});
         } else {
             res.status(401).send('Bad credentials');
         }
